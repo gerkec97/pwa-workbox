705d2c3afbd2c87ff513a34de2ebcb1212334b19
Gerrit Keck <gerrit.keck@globalgaming.com>
2021-01-06 16:07:12 +0100

    generic app shell. use unpkg.com for bootstrap and jquery

--- server/index.js
+++ server/index.js
@@ -10,8 +10,5 @@ const server = http.Server(app);
 // Server "app" directory
 app.use(express.static(`${__dirname}/../app`));
 
-// Server "node_modules" directory
-app.use('/modules', express.static(`${__dirname}/../node_modules`));
-
 // Start Server
 server.listen( 8000, () => console.log('Photo Message running on localhost:8000'));
